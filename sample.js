ad6ee8bb52db0c55f4a358f5217ddb4d118dfde5
jayeshm10 <141822696+jayeshm10@users.noreply.github.com>
2023-08-09 16:13:31 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,15 @@
+
+// const express = require("express")
+// const mongoose = require("mongoose")
+
+// const app = express()
+
+// app.listen(3000,() =>{
+
+//     console.log("server is listining")
+
+// })
+
+// app.get("/", (req, res, next) => {
+//     res.send(" its work ")
+// })
